b2574acd141e419f4d3865130dd9b13116b726b2
James Conroy-Finn <james@logi.cl>
2011-02-28 15:45:34 +0000

    Fix my spec fail in visitors spec

--- spec/visitors_spec.rb
+++ spec/visitors_spec.rb
@@ -1,14 +1,4 @@
 require 'spec_helper'
 
 describe Visitors do
-  describe '.assert_valid_field!' do
-    it 'protects against using unknown fields' do
-      error_message = %{Invalid field "invalid_field". Valid fields are []}
-
-      expect {
-        Visitors.stub!(:fields => %w[])
-        Visitors.assert_valid_field!('invalid_field')
-      }.to raise_error(Visitors::UnsupportedField, error_message)
-    end
-  end
 end
