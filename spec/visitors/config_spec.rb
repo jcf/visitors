8c791e1169b77955a9e7c674cf455005ea54b7fe
James Conroy-Finn <james@logi.cl>
2011-03-09 16:02:48 +0000

    Fix a spec for travis-ci

--- spec/visitors/config_spec.rb
+++ spec/visitors/config_spec.rb
@@ -5,13 +5,14 @@ describe Visitors::Config do
     it 'uses the config.yml file to return a hash of configuration information' do
       File.stub!(:expand_path => '/path/to/config.yml')
       YAML.should_receive(:load_file).with('/path/to/config.yml').and_return('development' => {})
+      Visitors.stub!(:env => 'development')
       Visitors::Config.load.all
     end
 
     it 'raises when an environment is not defined' do
       expect {
-	YAML.stub!(:load_file => {})
-	Visitors::Config.load.all
+        YAML.stub!(:load_file => {})
+        Visitors::Config.load.all
       }.to raise_error(Visitors::Config::MissingEnvironmentError)
     end
   end
